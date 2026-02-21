4ef0deedc3cd749fba7411c92c62c601b9aa521d
Stephen Abello <stephen.abello@combodo.com>
2026-02-21 14:19:49 +0100

    Fix file naming

--- steffunky-backoffice-dark-theme-base/module.steffunky-backoffice-dark-theme-base.php
+++ steffunky-backoffice-dark-theme-base/module.steffunky-backoffice-dark-theme-base.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'steffunky-backoffice-dark-theme-base/0.1.1',
+	'steffunky-backoffice-dark-theme-base/0.1.2',
 	array(
 		// Identification
 		//
